42129e1692e0eb76c8dbc3cce889a83b2628a378
dmancera7 <47235994+dmancera7@users.noreply.github.com>
2019-02-08 09:53:26 -0800

    Creacion del proyecto.

---
+++ Paquete/src/test/Pruebas.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Pruebas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Paquete1.Persona p = new Paquete1.Persona(23,"Maria");
+		Paquete2.Persona m = new Paquete2.Persona(20,"Jose","Masculino");
+		
+		System.out.println(p.toString());
+		System.out.println(m.toString());
+	}
+
+}
