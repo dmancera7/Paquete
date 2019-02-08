f86959542ca0f98ec1f7b02f27cabf0806d79f70
David Mancera <47235994+dmancera7@users.noreply.github.com>
2019-02-08 10:06:54 -0800

    Update Pruebas.java

--- Paquete/src/test/Pruebas.java
+++ Paquete/src/test/Pruebas.java
@@ -6,9 +6,10 @@ public class Pruebas {
 		// TODO Auto-generated method stub
 		Paquete1.Persona p = new Paquete1.Persona(23,"Maria");
 		Paquete2.Persona m = new Paquete2.Persona(20,"Jose","Masculino");
-		
+		System.out.println("INICIO DE IMPRESION");
 		System.out.println(p.toString());
 		System.out.println(m.toString());
+		System.out.println("FIN");
 	}
 
 }
